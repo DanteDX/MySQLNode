b40f982d624097d2ba2a2f010be3d4f66e8ec57f
dantedxwolf <teacher.martin@gmail.com>
2021-01-20 00:53:48 +0600

    encrypted

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'StarkD03',
+    password:'********',
     database:'nodeDataBase'
 });
 
